1f49949f2ef4797aa182c092584fe1738346b258
mahipal <mahipal_08@yahoo.co.in>
2014-05-11 20:14:30 +0530

    Adding plot2.R

---
+++ plot2.R
@@ -0,0 +1,20 @@
+# Exploratory Data Analysis
+# course project 
+# plot2
+
+rm(list=ls())
+#reading data into R:
+data<- read.table("household_power_consumption.txt", sep=";",nrows= 2075260, header=TRUE, quote= "", strip.white=TRUE, stringsAsFactors = FALSE, na.strings= "?")
+
+# Subsetting the full data to required date:  
+sub<- subset(data, (data$Date == "1/2/2007" | data$Date== "2/2/2007")) 
+
+# Changing the class of Date variable from character to Date: 
+sub$Date <- as.Date(sub$Date, format = "%d/%m/%Y")
+# Combining the Date and Time variable and creating a new column in dataset named "DateTime":
+sub$DateTime <- as.POSIXct(paste(sub$Date, sub$Time))
+
+# Creating the plot2:
+png("plot2.png", width=480, height= 480)
+plot(sub$DateTime, sub$Global_active_power, type= "l", lwd=1, ylab= "Global Active Power (kilowatts)", xlab="")
+dev.off()
